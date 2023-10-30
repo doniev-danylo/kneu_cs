a863f08abf4148602cbc3447a5b71b0fef91f506
Doniev_Danylo <danil.donev@appflame.com>
2023-10-30 10:36:35 +0200

    upd runner

--- lab2/runnerLab2.cs
+++ lab2/runnerLab2.cs
@@ -5,7 +5,7 @@ public class runnerLab2
     private static void Main()
     {
         Lab2Task1.RunTask1();
-        Lab2Task2.RunTask1(1);
+        Lab2Task2.RunTask2(1);
         Lab2Task3.RunTask3();
     }
 }
