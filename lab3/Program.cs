9bd3f411dc943a840e62f782176043f8e980c004
Doniev_Danylo <danil.donev@appflame.com>
2023-11-05 10:46:21 +0200

    add lab3

---
+++ lab3/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace lab2;
+
+public class runnerLab2
+{
+    private static void Main()
+    {
+        Lab2Task1.RunTask1();
+        Lab2Task2.RunTask2(1);
+        Lab2Task3.RunTask3();
+    }
+}
