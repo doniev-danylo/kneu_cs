5ed41ab49d4e9baae5941a2864895b4cfe0059eb
Doniev_Danylo <danil.donev@appflame.com>
2023-11-05 12:50:53 +0200

    Add TemperatureConverter to lab3 and update Program.cs
    In this commit, I've merged the TemperatureConverter feature, previously in a separate project, into the lab3 project. This change simplifies demonstration of the code producing the TemperatureConverter.dll. I also revised Program.cs to reflect a new implementation, using reflection to invoke the ConvertCelsiusToFahrenheit method from the TemperatureConverter class. Now it takes temperature in Celsius as input and prints out the corresponding temperature in Fahrenheit.

--- lab3/Program.cs
+++ lab3/Program.cs
@@ -1,9 +1,20 @@
-﻿using System;
+﻿using System.Reflection;
 
-class Program
+namespace ReflectionApp
 {
-    static void Main()
+    class Program
     {
-        Console.WriteLine("Hello, World!");
+        static void Main(string[] args)
+        {
+            Assembly assembly = Assembly.LoadFrom("/Users/donevd/Documents/КНЕУ/апбд/kneu_cs/lab3/TemperatureConverter.dll");
+            Type type = assembly.GetType("TemperatureConverter.TemperatureConverter");
+            MethodInfo method = type.GetMethod("ConvertCelsiusToFahrenheit");
+
+            double temperatureInCelsius = 20.0;
+            object result = method.Invoke(null, new object[] { temperatureInCelsius });
+
+            Console.WriteLine($"Температура у Цельсіях: {temperatureInCelsius}");
+            Console.WriteLine($"Температура у Фаренгейтах: {result}");
+        }
     }
 }
---
+++ lab3/TemperatureConverter.cs
@@ -0,0 +1,11 @@
+﻿// script moved from its own project with namespace TemperatureConverter
+// to demonstrate source code for build TemperatureConverter.dll
+namespace TemperatureConverter;
+
+public static class TemperatureConverter
+{
+    public static double ConvertCelsiusToFahrenheit(double celsius)
+    {
+        return (celsius * 9.0 / 5.0) + 32.0;
+    }
+}
